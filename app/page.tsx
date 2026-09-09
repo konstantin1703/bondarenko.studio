5be443f745e889fe77c71d8b0a31b29c5184e87a
konstantin1703 <irisinsightai@gmail.com>
2026-09-09 19:25:00 +0300

    fix(v9): connect mobile routes directly to hero panels

--- app/page.tsx
+++ app/page.tsx
@@ -49,20 +49,25 @@ export default function Home() {
             </g>
           </svg>
 
-          <svg className="v9-routes v9-routes--mobile" viewBox="0 0 390 760" preserveAspectRatio="none" aria-hidden="true">
+          <svg className="v9-routes v9-routes--mobile" viewBox="0 0 390 620" preserveAspectRatio="none" aria-hidden="true">
             <defs>
               <filter id="v9GlowMobile" x="-60%" y="-60%" width="220%" height="220%">
                 <feGaussianBlur stdDeviation="2.2" result="blur" />
                 <feMerge><feMergeNode in="blur" /><feMergeNode in="SourceGraphic" /></feMerge>
               </filter>
             </defs>
-            <g stroke="#55d8ff" strokeOpacity=".7" strokeWidth="1.25" fill="none" filter="url(#v9GlowMobile)">
-              <path d="M163 302 L118 248 L81 248" />
-              <path d="M168 309 L121 258 L81 258" />
-              <path d="M227 302 L272 248 L309 248" />
-              <path d="M222 309 L269 258 L309 258" />
-              <path d="M191 397 V480 L151 520" />
-              <path d="M199 397 V486 L239 520" />
+            <g stroke="#55d8ff" strokeOpacity=".74" strokeWidth="1.25" fill="none" filter="url(#v9GlowMobile)">
+              <path d="M82 286 H105 Q116 286 124 277 L164 232 Q169 226 176 226" />
+              <path d="M82 294 H109 Q120 294 128 285 L166 242 Q171 236 178 236" />
+              <path d="M82 302 H113 Q124 302 132 293 L168 252 Q173 246 180 246" />
+
+              <path d="M308 286 H285 Q274 286 266 277 L226 232 Q221 226 214 226" />
+              <path d="M308 294 H281 Q270 294 262 285 L224 242 Q219 236 212 236" />
+              <path d="M308 302 H277 Q266 302 258 293 L222 252 Q217 246 210 246" />
+
+              <path d="M188 405 V438 Q188 448 179 456 L170 464" />
+              <path d="M195 405 V466" />
+              <path d="M202 405 V438 Q202 448 211 456 L220 464" />
             </g>
           </svg>
 
